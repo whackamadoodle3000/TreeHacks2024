555a2c0cf64ea37d307c8ea949d305a0053cfaf7
Pranav Tadepalli <orca.pranav@gmail.com>
2024-02-17 13:57:42 -0800

    timestamp

--- ml-sensordata/get_data.py
+++ ml-sensordata/get_data.py
@@ -1,23 +1,20 @@
 import serial
 import csv
+import time
 
-
-def write_csv(filename, cervical_pressure, thoracic_pressure, lumbar_pressure, sacral_pressure):
-    # Define the headers and data rows
-    headers = ['Cervical_Pressure', 'Thoracic_Pressure', 'Lumbar_Pressure', 'Sacral_Pressure']
-    data = [[cervical_pressure, thoracic_pressure, lumbar_pressure, sacral_pressure]]
+def write_csv(filename, timestamp, cervical_pressure, thoracic_pressure, lumbar_pressure, sacral_pressure):
+    # Define the data rows
+    data = [[timestamp, cervical_pressure, thoracic_pressure, lumbar_pressure, sacral_pressure]]
     
-    # Write data to CSV file
-    with open(filename, mode='w', newline='') as file:
+    # Append data to CSV file
+    with open(filename, mode='a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(headers)
         writer.writerows(data)
 
-
 # Example usage:
-write_csv('pressure_data.csv', 10, 20, 30, 40)
+write_csv('pressure_data.csv', 0, 10, 20, 30, 40)  # Initial example data
 
-# Define the serial port (COM port) and baud rate
+# Define the serial port and baud rate
 serial_port = '/dev/tty.usbmodem1302'  # Change this to your Arduino's COM port
 baud_rate = 9600  # Make sure this matches your Arduino's baud rate
 
@@ -29,8 +26,9 @@ try:
     while True:
         # Read one line of data from the serial port
         line = ser.readline().decode().strip()
-        port_a, port_b, port_c = list(map(int,line.split(",")))
-        write_csv("sensor_data.csv", port_a, port_b, port_c, port_c)
+        port_a, port_b, port_c, port_d = list(map(int,line.split(",")))
+        timestamp = int(time.time())  # Get current timestamp in seconds
+        write_csv("sensor_data.csv", timestamp, port_a, port_b, port_c, port_d)
         # Print the received data
         print("Received:", line)
 
