a3a2748cf582fdf6481680d29a84f8ac8f9aa02a
Pranav Tadepalli <orca.pranav@gmail.com>
2024-02-18 05:15:07 -0800

    stylistic changes

--- chair/src/components/Main.js
+++ chair/src/components/Main.js
@@ -76,89 +76,102 @@ function Main() {
             clearInterval(intervalId);
         };
     }, []);
-  return (
-    <div className="App">
-        <header className="main-header">
-            <Grid container spacing={2} className='grid'>
-                <Grid item xs={12} >
-                    <h1 className='main-title'>CH</h1>
-                    <h1 className='main-title gradient'>AI</h1>
-                    <h1 className='main-title'>R</h1>
-                </Grid>
-                <Grid item xs={6} className='grid-item'>
-                    <div className='grid-content'>
-                    <div className="spine-wrapper">
-                    <VictoryChart width={400} height={400}>
-                        <VictoryLabel x={200} y={30} text="Posture Scoring" textAnchor="middle" />
-                        <VictoryAxis
-                            label="Time (s)"
-                            style={{
-                                axisLabel: { padding: 30 } // Adjust the padding as needed
-                            }}
-                        />
+    return (
+        <div className="App">
+            <header className="main-header">
+                <Grid container spacing={2} className='grid'>
+                    <Grid item xs={12} >
+                        <h1 className='main-title'>CH</h1>
+                        <h1 className='main-title gradient'>AI</h1>
+                        <h1 className='main-title'>R</h1>
+                    </Grid>
+                    <Grid item xs={6} className='grid-item victory-chart'>
+                        <div className='grid-content'>
+                            <div className="spine-wrapper" >
+                                <VictoryChart width={800} height={300}>
+                                    {/*<VictoryLabel x={200} y={30} text="Posture Scoring" style={{
+                                        fill: "white",
+                                        fontSize: 11,
+                                    }} textAnchor="middle" />*/}
+                                    <VictoryAxis
+                                        label="Time (s)"
+                                        style={{
+                                            axisLabel: { padding: 40, fill: "white" , fontSize: 11,},
+                                            axis: { stroke: "white" }, // Makes the axis line white
+                                            ticks: { stroke: "white" }, // Makes the ticks white
+                                            tickLabels: { fill: "white",  },
+                                        }}
+                                    />
 
-                        {/* Add the Y Axis with a title */}
-                        <VictoryAxis
-                            dependentAxis
-                            label="Alignment Score"
-                            style={{
-                                axisLabel: { padding: 40 } // Adjust the padding as needed
-                            }}
-                        />
+                                    {/* Add the Y Axis with a title */}
+                                    <VictoryAxis
+                                        dependentAxis
+                                        label="Alignment Score"
+                                        style={{
+                                            axisLabel: { padding: 40 , fill: "white", fontSize: 11,},
+                                            axis: { stroke: "white" }, // Makes the axis line white
+                                            ticks: { stroke: "white" }, // Makes the ticks white
+                                            tickLabels: { fill: "white", },
+                                        }}
+                                    />
 
-                        {/* Add the VictoryLegend component */}
-                        <VictoryLegend x={400} y={50}
-                            title="Legend"
-                            centerTitle
-                            orientation="horizontal"
-                            gutter={20}
-                            style={{ border: { stroke: "black" }, title: { fontSize: 10 } }}
-                            data={[
-                                { name: "Shoulder", symbol: { fill: "#c84d8f" } },
-                                { name: "Back", symbol: { fill: "#4b0082" } }
-                            ]}
-                        />
+                                    {/* Add the VictoryLegend component */}
+                                    <VictoryLegend x={600} y={0}
+                                        title="Legend"
+                                        centerTitle
+                                        orientation="horizontal"
+                                        gutter={20}
+                                        style={{ 
+                                            border: { stroke: "white" }, 
+                                            title: { fontSize: 10, fill: "white" }, // Set title text color to white
+                                            labels: { fill: "white" } // Set legend item labels to white
+                                        }}
+                                        data={[
+                                            { name: "Shoulder", symbol: { fill: "#c84d8f" } },
+                                            { name: "Back", symbol: { fill: "#4b0082" } }
+                                        ]}
+                                    />
 
-                        <VictoryLine data={postureDataRef.current} x="name" y="shoulder_align" style={{ data: { stroke: "#c84d8f" } }} />
-                        <VictoryLine data={postureDataRef.current} x="name" y="back_align" style={{ data: { stroke: "#4b0082" } }} />
-                    </VictoryChart>
-                </div>
-                        <p className='caption'>Posture scores</p>
-                    </div>
+                                    <VictoryLine data={postureDataRef.current} x="name" y="shoulder_align" style={{ data: { stroke: "#c84d8f" }, fill: "white" }} />
+                                    <VictoryLine data={postureDataRef.current} x="name" y="back_align" style={{ data: { stroke: "#4b0082" }, fill: "white" }} />
+                                </VictoryChart>
+                            </div>
+                            <p className='caption'>Posture scores</p>
+                        </div>
+                    </Grid>
+                    <Grid item xs={3} className='grid-item'>
+                        <div className='grid-content'>
+                            <img className='skeleton' src={`${image}?${new Date().getTime()}`} />
+                            <p className='caption'>Skeleton visualization</p>
+                        </div>
+                    </Grid>
+                    <Grid item xs={3} className='grid-item'>
+                        <div className='grid-content'>
+                            <Spine className='spine' spinePoint={spinePointRef} />
+                            <p className='caption'>Spine pressure map</p>
+                        </div>
+                    </Grid>
+                    <Grid item xs={6} className='grid-item'>
+                        <div className='grid-content'>
+                            <p className='caption'>Explore how posture affects overall health</p>
+                            {gptRecRef === "" && <FaStopwatch className='timer' />}
+                            {!gptRecRef === "" && <p className='gpt-content'> {gptRecRef.current} </p>}
+                        </div>
+                    </Grid>
+                    <Grid item xs={6} className='grid-item'>
+                        <div className='grid-content'>
+                            <p className='large-text'> Chair is a data visualization platform built for a smart chair equipped with pressure sensors and a webcam, whichuses a combination of deep learning and custom regression models to analyze and correct improper posture while sitting in a chair.
+                                <br /><br />
+                                We equipped a chair with 4 pressure sensors lining the seat back. Using real-time sensor data, we trained a custom deep learning neural network to detect how crooked the spine was when sitting, and identify the most pressurized areas of the spine. The spine animation to the left is an interactive visualization of the most pressurized segments of the spine, where green indicates low and red indicates high pressure. The site also renders a beep to alert the user of poor posture when detected.
+                                <br /><br />
+                                In the webcam above, we use computer vision techniques to detect and render real-time pose estimations. We overlay the pose estimations on top of the live webcam feed. The line chart to the right displays historical posture scores from the past hour which analyze neck-to-head, back, and shoulder alignment. The higher the score, the better the alignment.</p>
+                            <p className='gptContent'></p>
+                        </div>
+                    </Grid>
                 </Grid>
-                <Grid item xs={3} className='grid-item'>
-                    <div className='grid-content'>
-                        <img className='skeleton' src={`${image}?${new Date().getTime()}`} />
-                        <p className='caption'>Skeleton visualization</p>
-                    </div>
-                </Grid>
-                <Grid item xs={3} className='grid-item'>
-                    <div className='grid-content'>
-                        <Spine className='spine' spinePoint={spinePointRef} />
-                        <p className='caption'>Spine pressure map</p>
-                    </div>
-                </Grid>
-                <Grid item xs={6} className='grid-item'>
-                    <div className='grid-content'>
-                        <p className='caption'>Explore how posture affects overall health</p>
-                        {gptRecRef==="" && <FaStopwatch className='timer'/>}
-                        {!gptRecRef==="" && <p className='gpt-content'></p>}
-                    </div>
-                </Grid>
-                <Grid item xs={6} className='grid-item'>
-                    <div className='grid-content'>
-                        <p className='large-text'> Chair is a data visualization platform built for a smart chair equipped with pressure sensors and a webcam, whichuses a combination of deep learning and custom regression models to analyze and correct improper posture while sitting in a chair.
-                                                <br/><br/>
-                                                We equipped a chair with 4 pressure sensors lining the seat back. Using real-time sensor data, we trained a custom deep learning neural network to detect how crooked the spine was when sitting, and identify the most pressurized areas of the spine. The spine animation to the left is an interactive visualization of the most pressurized segments of the spine, where green indicates low and red indicates high pressure. The site also renders a beep to alert the user of poor posture when detected.
-                                                <br/><br/>
-                                                In the webcam above, we use computer vision techniques to detect and render real-time pose estimations. We overlay the pose estimations on top of the live webcam feed. The line chart to the right displays historical posture scores from the past hour which analyze neck-to-head, back, and shoulder alignment. The higher the score, the better the alignment.</p>
-                        <p className='gptContent'></p>
-                    </div>
-                </Grid>
-            </Grid>
-            
-        </header>
-    </div >
-  );
+
+            </header>
+        </div >
+    );
 }
 export default Main;
